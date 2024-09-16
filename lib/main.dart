50facff70f781074f8199ec39384cb5ec5f3df94
Yash <yashwant.kumar@codeclouds.co.in>
2024-09-16 17:53:56 +0530

    Made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:zioks_application/image_mapper.dart';
-import 'package:zioks_application/pages/confirmation_page.dart';
-=======
-import 'user_photo.dart';
->>>>>>> d27f422055660f039c7e05b8e66dd1095e952ac4
+import 'pages/user_photo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-      title: "Zioks-CheckApplication",
-      home: ConfirmationPage(text: "Kshitij Jaiswal\n$exitnote"),
-=======
     return  MaterialApp(
       home: UserPhoto(),
->>>>>>> d27f422055660f039c7e05b8e66dd1095e952ac4
     );
   }
 }
