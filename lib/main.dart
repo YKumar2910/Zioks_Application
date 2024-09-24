ec3dea659181a20bd545588c33eba2785320a8bf
Yash <yashwant.kumar@codeclouds.co.in>
2024-09-24 10:43:13 +0530

    Routes.dart added...

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'pages/user_photo.dart';
 import 'pages/purposepage.dart';
 import 'pages/deatilspage.dart';
 import 'package:zioks_application/pages/confirmation_page.dart';
+import 'routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: Details(),);
+      home: Details(),
+      
+      initialRoute: MyRoutes.userphotoRoute,
+      routes: {
+      MyRoutes.userphotoRoute: (context) => UserPhoto(),
+  },
+
+      );
   }
 }
